39f5546f956a4e68fc714f2179337be16d8d2e56
Zilde Souto Maior Neto <zsmn@ug3c14.windows.cin.ufpe.br>
2019-08-15 11:00:16 -0300

    added graphics

--- euler.py
+++ euler.py
@@ -1,18 +1,37 @@
 import math
+import matplotlib.pyplot as graphic
 
-u = [] # vetor com os valores 
+arry = [] # vetor com os resultados
+arrt = [] # vetor com os passos
+arrs = [] # vetor com os resultados exatos (solução da edo)
+
 def f(t, y): return (1 - t + (4 * y)) # y(t, y)
+def solve(t): return ((1/4)*t - (3/16) + (19/16)*math.exp(4*t)) # solucao da EDO
 
 n = int(input()) # quantidade de pontos
 h = float(input()) # valor do passo
 
-u.append(1) # caso base
+arry.append(1) # caso base
+arrs.append(1) # caso base da EDO
+
 qt_it = math.ceil(n/h) # quantidade de iterações necessarias
 
 t = 0 # t vai de 0 até n (0,n)
+arrt.append(t) # iniciando com o primeiro passo
 
 for i in range(1, qt_it+1):
-    u.append(u[i-1] + h * f(t, u[i-1])) # metodo de euler
-    t = t + h
+    arry.append(arry[i-1] + h * f(t, arry[i-1])) # metodo de euler
+    arrs.append(solve(t)) # solucao da EDO
+    t = t + h # incrementando o passo
+    arrt.append(t) # colocando o passo no array
 
-print("Result = {}".format(u[qt_it]))
+# plotagem do grafico 
+graphic.title('Euler Method')
+graphic.xlabel("Passos")
+graphic.ylabel("f(t, y)")
+
+graphic.plot(arrt, arry, color = 'red') # cor vermelha pra o grafico da sol. de euler
+graphic.plot(arrt, arrs, color = 'yellow') #cor amarela para o grafico da sol. da EDO
+
+print("Result = {}".format(arry[qt_it]))
+graphic.show()
