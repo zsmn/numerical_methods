55504180a82b5a8fa73dbe5a97fecd5e9e7f7f23
zsmn <zsmn@cin.ufpe.br>
2019-10-20 10:04:42 -0300

    formula inversa inserida

--- methods.py
+++ methods.py
@@ -29,6 +29,24 @@ consts_multon = [
 	[5257.0/17280.0,139849.0/120960.0,-4511.0/4480.0,123133.0/120960.0,-88547.0/120960.0,1537.0/4480.0,-11351.0/120960.0,275.0/24192.0]
 ]
 
+consts_inversa_before = [
+    [1.0],
+    [4.0/3.0, -1.0/3.0],
+    [18.0/11.0, -9.0/11.0, 2.0/11.0],
+    [48.0/25.0,-36.0/25.0,16.0/25.0,-3.0/25.0],
+	[300.0/137.0,-300.0/137.0,200.0/137.0,-75.0/137.0,12.0/137.0],
+	[360.0/147.0,-450.0/147.0,400.0/147.0,-225.0/147.0,72.0/147.0,-10.0/147.0]
+]
+
+consts_inversa_after = [
+    [1.0],
+	[2.0/3.0],
+	[6.0/11.0],
+	[12.0/25.0],
+	[60.0/137.0],
+	[60.0/147.0]
+]
+
 def plotGraphic(method, arrt, arry, color):
     graphic.title(method)
     graphic.xlabel("Passos")
@@ -40,7 +58,7 @@ def plotGraphic(method, arrt, arry, color):
 
     return
 
-def euler(funct, y0, t0, qt_it, h):
+def euler(funct, y0, t0, qt_it, h, printar = True):
     print('Euler')
     print('y({}) = {}'.format(t0, y0))
     print('it = {}'.format(qt_it))
@@ -54,7 +72,7 @@ def euler(funct, y0, t0, qt_it, h):
     arry.append(float(y0))
 
     for i in range(1, int(qt_it)+2):
-        print(i-1, ' ', arry[i-1])
+        if(printar): print(i-1, ' ', arry[i-1])
         arry.append(arry[i-1] + float(h) * f.subs(t, arrt[i-1]).subs(y, arry[i-1]))
         arrt.append(arrt[i-1] + float(h))
 
@@ -74,7 +92,7 @@ def reverse_euler(funct, y0, t0, qt_it, h):
     arry.append(float(y0))
 
     for i in range(0, int(qt_it) + 1):
-        print(i, ' ', arry[i])
+        if(printar): print(i, ' ', arry[i])
         yaux = arry[i] + float(h) * f.subs(t, arrt[i]).subs(y, arry[i])
         taux = arrt[i] + float(h)
 
@@ -97,7 +115,7 @@ def aprimorated_euler(funct, y0, t0, qt_it, h):
     arry.append(float(y0))
 
     for i in range(1, int(qt_it)+2):
-        print(i - 1, ' ', arry[i])
+        if(printar): print(i - 1, ' ', arry[i-1])
         k1 = f.subs(t, arrt[i-1]).subs(y, arry[i-1])
         k2 = f.subs(t, arrt[i-1]+float(h)).subs(y, arry[i-1] + float(h) * k1)
         arry.append(arry[i-1] + (float(h)/2) * (k1 + k2))
@@ -119,7 +137,7 @@ def runge_kutta(funct, y0, t0, qt_it, h):
     arry.append(float(y0))
 
     for i in range(1, int(qt_it)+2):
-        print(i - 1, ' ', arry[i-1])
+        if(printar): print(i - 1, ' ', arry[i-1])
         k1 = f.subs(t, arrt[i-1]).subs(y, arry[i-1])
         k2 = f.subs(t, arrt[i-1] + 0.5 * float(h)).subs(y, arry[i-1] + 0.5 * float(h) * k1)
         k3 = f.subs(t, arrt[i-1] + 0.5 * float(h)).subs(y, arry[i-1] + 0.5 * float(h) * k2)
@@ -129,7 +147,7 @@ def runge_kutta(funct, y0, t0, qt_it, h):
 
     return arry, arrt
 
-def bashfort_dynamic_p(funct, vety, tf, h, ordem):
+def bashfort_dynamic_prevision(funct, vety, tf, h, ordem):
     aux = 0.0
     yf = vety[len(vety) - 1]
 
@@ -156,7 +174,8 @@ def adam_multon(funct, vety, vett, qt_it, h, ordem, complemento = ''):
 
     for i in range(int(ordem), int(qt_it) + 1, 1):
         aux = 0.0
-        pd_term = float(h) * consts_multon[int(ordem) - 1][0] * f.subs(t, float(arrt[i - 1]) + float(h)).subs(y, bashfort_dynamic_p(funct, arry, arrt[len(arrt) - 1], h, ordem))
+        # calculando com ajuda de bashforth
+        pd_term = float(h) * consts_multon[int(ordem) - 1][0] * f.subs(t, float(arrt[i - 1]) + float(h)).subs(y, bashfort_dynamic_prevision(funct, arry, arrt[len(arrt) - 1], h, ordem))
         aux += pd_term
 
         for j in range(1, len(consts_multon[int(ordem) - 1]), 1):
@@ -198,6 +217,34 @@ def adam_bashforth(funct, vety, vett, qt_it, h, ordem, complemento = ''):
 
 ## falta formula inversa!
 
+def formula_inversa(funct, vety, vett, qt_it, h, ordem, complemento = ''):
+    print('Formula Inversa', complemento)
+    print('it = {}'.format(qt_it))
+    print('h = {}'.format(h))
+
+    f = parse_expr(funct)
+    arrt = vett
+    arry = vety
+
+    for i in range(len(arry)):
+        print(i, ' ', arry[i])
+
+    for i in range(int(ordem), int(qt_it) + 1, 1):
+        aux = 0.0
+
+        # previsao com bashforth
+        aux += float(h) * consts_inversa_after[int(ordem) - 1][0] * f.subs(y, bashfort_dynamic_prevision(funct, arry, arrt[len(arrt) - 1], h, ordem)).subs(t, arrt[i - 1] + float(h))
+
+        for j in range(0, len(consts_inversa_before[int(ordem) - 1]), 1):
+            aux += consts_inversa_before[int(ordem) - 1][j] * arry[len(arry) - j - 1]
+
+        arry.append(aux)
+        arrt.append(float(arrt[i - 1]) + float(h))
+
+        print(i, ' ', arry[i])
+
+    return arry, arrt
+
 def main():
     arq = open('input.txt', 'r')
 
@@ -216,8 +263,11 @@ def main():
     # adam_bashforth
     # metodo y0 y1 ... yn t0 qt_passos func ordem
 
-    # formula inversa
-    # ?
+    # formula_inversa_by_euler, by_euler_inverso, by_runge_kutta, by_euler_aprimorado
+    # metodo y0 t0 qt_passos func ordem
+
+    # formula_inversa
+    # metodo y0 y1 .... yn t0 qt_passos func ordem
      
 
     for linha in arq:
@@ -236,19 +286,19 @@ def main():
             plotGraphic("Runge Kutta", pts_t, pts_y, 'black')
         elif("adam_multon" in param[0]):
             if("by_euler_inverso" in param[0]):
-                pts_y, pts_t = reverse_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3])
+                pts_y, pts_t = reverse_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
                 pts_y, pts_t = adam_multon(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler Inverso')
                 plotGraphic("Adam Multon by Euler Inverso", pts_t, pts_y, 'cyan')
             elif("by_euler_aprimorado" in param[0]):
-                pts_y, pts_t = aprimorated_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3])
+                pts_y, pts_t = aprimorated_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
                 pts_y, pts_t = adam_multon(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler Aprimorado')
                 plotGraphic("Adam Multon by Euler Aprimorado", pts_t, pts_y, 'cyan')
             elif("by_runge_kutta" in param[0]):
-                pts_y, pts_t = runge_kutta(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3])
+                pts_y, pts_t = runge_kutta(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
                 pts_y, pts_t = adam_multon(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Runge Kutta')
                 plotGraphic("Adam Multon by Runge Kutta", pts_t, pts_y, 'cyan')
             elif("by_euler" in param[0]):
-                pts_y, pts_t = euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3])
+                pts_y, pts_t = euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
                 pts_y, pts_t = adam_multon(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler')
                 plotGraphic("Adam Multon by Euler", pts_t, pts_y, 'cyan')
             else:
@@ -264,19 +314,19 @@ def main():
                 plotGraphic("Adam Multon", pts_t, pts_y, 'cyan')
         elif("adam_bashforth" in param[0]):
             if("by_euler_inverso" in param[0]):
-                pts_y, pts_t = reverse_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3])
+                pts_y, pts_t = reverse_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
                 pts_y, pts_t = adam_bashforth(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler Inverso')
                 plotGraphic("Adam Bashforth by Euler Inverso", pts_t, pts_y, 'brown')
             elif("by_euler_aprimorado" in param[0]):
-                pts_y, pts_t = aprimorated_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3])
+                pts_y, pts_t = aprimorated_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
                 pts_y, pts_t = adam_bashforth(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler Aprimorado')
                 plotGraphic("Adam Bashforth by Euler Aprimorado", pts_t, pts_y, 'brown')
             elif("by_runge_kutta" in param[0]):
-                pts_y, pts_t = runge_kutta(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3])
+                pts_y, pts_t = runge_kutta(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
                 pts_y, pts_t = adam_bashforth(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Runge Kutta')
                 plotGraphic("Adam Bashforth by Runge Kutta", pts_t, pts_y, 'brown')
             elif("by_euler" in param[0]):
-                pts_y, pts_t = euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3])
+                pts_y, pts_t = euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
                 pts_y, pts_t = adam_bashforth(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler')
                 plotGraphic("Adam Bashforth by Euler", pts_t, pts_y, 'brown')
             else:
@@ -290,5 +340,32 @@ def main():
                 
                 pts_y, pts_t = adam_bashforth(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1])
                 plotGraphic("Adam Bashforth", pts_t, pts_y, 'brown')
-
+        elif("formula_inversa" in param[0]):
+            if("by_euler_inverso" in param[0]):
+                pts_y, pts_t = reverse_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
+                pts_y, pts_t = formula_inversa(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler Inverso')
+                plotGraphic("Formula Inversa by Euler Inverso", pts_t, pts_y, 'silver')
+            elif("by_euler_aprimorado" in param[0]):
+                pts_y, pts_t = aprimorated_euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
+                pts_y, pts_t = formula_inversa(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler Aprimorado')
+                plotGraphic("Formula Inversaby Euler Aprimorado", pts_t, pts_y, 'silver')
+            elif("by_runge_kutta" in param[0]):
+                pts_y, pts_t = runge_kutta(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
+                pts_y, pts_t = formula_inversa(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Runge Kutta')
+                plotGraphic("Formula Inversa by Runge Kutta", pts_t, pts_y, 'silver')
+            elif("by_euler" in param[0]):
+                pts_y, pts_t = euler(param[len(param) - 2], param[1], param[2], int(param[len(param) - 1]) - 2, param[3], False)
+                pts_y, pts_t = formula_inversa(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1], ' by Euler')
+                plotGraphic("Formula Inversa by Euler", pts_t, pts_y, 'silver')
+            else:
+                pts_y = []
+                pts_t = []
+                h_aux = 0
+                for i in range(int(param[len(param) - 1])):
+                    pts_y.append(float(param[i+1]))
+                    pts_t.append(float(param[len(param) - 4]) + h_aux)
+                    h_aux = h_aux + float(param[len(param) - 4])
+                
+                pts_y, pts_t = formula_inversa(param[len(param) - 2], pts_y, pts_t, param[len(param) - 3], param[len(param) - 4], param[len(param) - 1])
+                plotGraphic("Formula Inversa", pts_t, pts_y, 'silver')
 main()
